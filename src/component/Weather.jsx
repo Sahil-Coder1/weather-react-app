da80b3a5696cc1ac8f3bdecaa279280e2bee6db8
Sahil-Coder1 <26maysahil@gmail.com>
2024-11-05 20:27:09 +0530

    updated

--- src/component/Weather.jsx
+++ src/component/Weather.jsx
@@ -23,7 +23,7 @@ const Weather = () => {
       method: "POST",
       headers: {
         Authorization: `Bearer ${apiKey}`,
-        "Content-Type": "application/json",
+        "Content-Type":"application/json",
       },
       body: JSON.stringify(prompt),
     });
